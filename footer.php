af6ecc1bf0aef119361d7aa49b3de7bc45f9bab7
sabuzwebdev <frsmgt@gmail.com>
2018-06-12 20:15:11 +0600

    All file is set up for wp api with Vue.js

---
+++ footer.php
@@ -0,0 +1,9 @@
+
+	
+	<?php wp_footer(); ?>
+	<script src="<?php bloginfo('template_url'); ?>/js/vue.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/vue-resource.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/vue-router.js"></script>
+	<script src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
+</body>
+</html>
